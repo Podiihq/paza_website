f8ce6278758215cb8ccf494e73902c5b82b42101
Parlgy <parlg4776@gmail.com>
2026-02-04 15:37:09 +0300

    dolby about page

--- src/pages/about_page/AboutPage.jsx
+++ src/pages/about_page/AboutPage.jsx
@@ -9,6 +9,7 @@ import Drawer from '../../components/DrawerComponent'
 import StudioVideo from "../../assets/images/studio/Paza_Studio.mp4"
 import logo1 from "../../assets/logos/studio_logo/paza.png"
 import logo2 from "../../assets/logos/studio_logo/abh.png"
+import logo3 from "../../assets/logos/studio_logo/DOLBY_logo.png"
 import ArtistCardCarousel from '../../components/ArtistCard'
 import CompanyCardCarousel from '../../components/CompanyCard'
 
@@ -39,27 +40,25 @@ const AboutPage = () => {
             <div className='text-white bg-black'>
                 <NavBar />
                 <div className="pt-32 lg:max-w-screen-2xl mx-auto px-4">
-                    <p className="text-[170px] leading-none lg:text-[400px] lg:leading-90 uppercase text-[#9EFF50]">About Us</p>
-                    <div className="">
-                        <div />
-                        <div className="col-span-2">
-                            <div className='flex justify-center mb-20'>
-                                <div className='lg:w-1/2'>
-                                    <p className="helvetica-light lg:text-xl pt-5 lg:pt-10">
-                                        We’re a hands-on audio-visual production studio that craft complete brand experiencesfrom initial concept to final delivery in any format. We’re a hands-on audio-visual production studio that craft complete brand experiencesfrom initial concept to final delivery in any format.
-                                        We’re a hands-on audio-visual production studio that craft complete brand experiencesfrom initial concept to final delivery in any format. We’re a hands-on audio-visual production studio that craft complete brand experiencesfrom initial concept to final delivery in any format.</p>
-                                    <div className='flex items-center gap-10 pt-10'>
-                                        <img src={logo1} alt="" className='w-20' />
-                                        <img src={logo2} alt="" className='h-20 border border-[#6F6F6F]' />
-                                    </div>
+                    <p className="text-[170px] leading-none lg:text-[500px] lg:leading-90 uppercase text-[#9EFF50]">About Us</p>
+                    <div className="mt-10">
+                        <div className='flex justify-center mb-20'>
+                            <div className='lg:w-2/3'>
+                                <p className="helvetica-light lg:text-xl pt-5 lg:pt-10">
+                                    We’re a hands-on audio-visual production studio that craft complete brand experiencesfrom initial concept to final delivery in any format. We’re a hands-on audio-visual production studio that craft complete brand experiencesfrom initial concept to final delivery in any format.
+                                    We’re a hands-on audio-visual production studio that craft complete brand experiencesfrom initial concept to final delivery in any format. We’re a hands-on audio-visual production studio that craft complete brand experiencesfrom initial concept to final delivery in any format.</p>
+                                <div className='flex items-center flex-wrap gap-10 pt-10 justify-center lg:justify-normal'>
+                                    <img src={logo1} alt="" className='w-20' />
+                                    <img src={logo2} alt="" className='h-20 border border-[#1a1a1a]' />
+                                    <img src={logo3} alt="" className='h-20 border border-[#1a1a1a]' />
                                 </div>
                             </div>
-                            <div className='relative'>
-                                <video autoPlay loop muted className='mx-auto lg:w-1/2 h-full object-cover z-10'>
-                                    <source src={StudioVideo} type="video/mp4" />
-                                </video>
-                                {/* <img src={StudioImage} alt="" className='lg:w-1/2 mx-auto' /> */}
-                            </div>
+                        </div>
+                        <div className='relative'>
+                            <video autoPlay loop muted className='mx-auto lg:w-2/3 h-full object-cover z-10'>
+                                <source src={StudioVideo} type="video/mp4" />
+                            </video>
+                            {/* <img src={StudioImage} alt="" className='lg:w-1/2 mx-auto' /> */}
                         </div>
                     </div>
                 </div>
--- src/pages/landing_page/LoadingPage.jsx
+++ src/pages/landing_page/LoadingPage.jsx
@@ -1,27 +1,7 @@
-import gsap from 'gsap';
-import React, { useState } from 'react'
 
 const LoadingPage = () => {
-    const [loaderFinished, setLoaderFinished] = useState(false);
-    const [timeline, setTimeline] = useState(null);
-
-    useLayoutEffect(() => {
-        const context = gsap.context(() => {
-            const tl = gsap.timeline({
-                onComplete: () => setLoaderFinished(true)
-            });
-
-            setTimeline(tl);
-        });
-
-        return () => context.revert();
-
-    }, [])
-
     return (
-        <div>
-            {loaderFinished ? <HomePage /> : <LoaderPage timeline={timeline} />}
-        </div>
+        <div>LoadingPage</div>
     )
 }
 
